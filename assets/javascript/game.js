a71d5b4409b71defbe21da7ba4d2fdebd2e91cb4
code-guy21 <alexissj16@knights.ucf.edu>
2020-04-22 07:48:40 -0400

    added jQuery to hide other characters when main character is selected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,34 @@
 $(document).ready(function () {
+	const characters = [
+		{
+			name: 'Obi-Wan Kenobi',
+			health: 120,
+		},
+		{
+			name: 'Luke Skywalker',
+			health: 100,
+		},
+		{
+			name: 'Dark Sidious',
+			health: 150,
+		},
+		{
+			name: 'Darth Maul',
+			health: 180,
+		},
+	];
+
 	let main;
 
 	$('.player').click(function () {
 		if (!main) {
 			main = $(this).val();
-			console.log(main);
+
+			let mainElement = $('<button>')
+				.text(characters[main].name)
+				.attr('class', 'players');
+
+			$('#players').html(mainElement);
 		}
 	});
 });
